3f1635c6a54434cf0f6f5c614b9f9c810dfbaa87
ehdwo13 <ehdwo13@gmail.com>
2024-04-12 17:46:04 +0900

    20240412

---
+++ jsp_study/src/main/java/service/BoardServiceImp.java
@@ -0,0 +1,52 @@
+package service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import domain.BoardVO;
+import repository.BoardDAO;
+import repository.BoardDAOImpl;
+
+public class BoardServiceImp implements BoardService {
+	
+	private static final Logger log = LoggerFactory.getLogger(BoardServiceImp.class);
+	private BoardDAO bdao;
+	
+	public BoardServiceImp() {
+		bdao = new BoardDAOImpl();
+	}
+
+	@Override
+	public int register(BoardVO bvo) {
+		log.info("register service in");
+		return bdao.insert(bvo);
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		log.info("getList service in");
+		return bdao.selectList();
+	}
+
+	@Override
+	public BoardVO getDetail(int bno) {
+		log.info("getDetail service in");
+		return bdao.selectOne(bno);
+	}
+
+	@Override
+	public int update(BoardVO bvo) {
+		log.info("update service in");
+		return bdao.update(bvo);
+	}
+
+	@Override
+	public int delete(int bno) {
+		log.info("delete service in");
+		return bdao.delete(bno);
+	}
+
+	
+}
---
+++ jsp_study/src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>hello world~!</h1>
+	
+	<h3>
+	<a href="/brd/register">글쓰기 페이지로...</a>
+	<a href="/brd/list">리스트 페이지로...</a>
+	
+	</h3>
+</body>
+</html>
