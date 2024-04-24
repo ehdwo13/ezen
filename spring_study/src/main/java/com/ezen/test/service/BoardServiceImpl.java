5810c9dc5a762d1536858250f1faa5cb87405e83
ehdwo13 <ehdwo13@gmail.com>
2024-04-24 16:02:43 +0900

    20240424

--- spring_study/src/main/java/com/ezen/test/service/BoardServiceImpl.java
+++ spring_study/src/main/java/com/ezen/test/service/BoardServiceImpl.java
@@ -1,17 +1,50 @@
 package com.ezen.test.service;
 
-import javax.inject.Inject;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.ezen.test.domain.BoardVO;
 import com.ezen.test.repository.BoardDAO;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class BoardServiceImpl implements BoardService {
 	
-	@Inject
-	private BoardDAO bdao;
+
+	private final BoardDAO bdao;
+
+	@Override
+	public int insert(BoardVO bvo) {
+		log.info("board register service check");
+		return bdao.insert(bvo);
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		log.info("board list service check");
+		return bdao.getList();
+	}
+
+	@Override
+	public BoardVO getDetail(int bno) {
+		log.info("board detail service check");
+		return bdao.getDetail(bno);
+	}
+
+	@Override
+	public void update(BoardVO bvo) {
+		log.info("board modify service check");
+		bdao.update(bvo);
+	}
+
+	@Override
+	public void remove(BoardVO bvo) {
+		log.info("board remove service check");
+		bdao.updateIsDel(bvo);
+	}
 }
--- spring_study/src/main/webapp/WEB-INF/views/layout/header.jsp
+++ spring_study/src/main/webapp/WEB-INF/views/layout/header.jsp
@@ -18,13 +18,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">게시판 글쓰기</a>
+          <a class="nav-link" href="/board/register">게시판 글쓰기</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">게시판 보기</a>
+          <a class="nav-link" href="/board/list">게시판 보기</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">회원가입</a>
