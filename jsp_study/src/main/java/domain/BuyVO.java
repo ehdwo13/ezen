0055496cda444c836e23e92781c40ef1d8493239
ehdwo13 <ehdwo13@gmail.com>
2024-04-27 12:00:00 +0900

    20240427

---
+++ jsp_study/src/main/java/domain/BuyVO.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class BuyVO {
+	private int bnum;
+	private String customer;
+	private String product_name;
+	private int price;
+	private int amount;
+	private String buydate;
+}
