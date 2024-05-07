1e4dec5167db052fb33fbb6281da1ac3ca48119b
ehdwo13 <ehdwo13@gmail.com>
2024-05-07 17:17:37 +0900

    20240507

---
+++ spring_test2/src/main/java/com/ezen/www/config/SecurityInitalizer.java
@@ -0,0 +1,8 @@
+package com.ezen.www.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+//상속받아 시큐리티 관련 필터 활성
+public class SecurityInitalizer extends AbstractSecurityWebApplicationInitializer {
+	
+}
---
+++ spring_test2/src/main/java/com/ezen/www/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.ezen.www.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ezen.www.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@Controller
+@Slf4j
+@RequestMapping("/user/**")
+public class UserController {
+	private final UserService usv;
+}
